c3904c2d05ca80ba8efa85ea520a83b12c058b35
psetman <psetman@gmail.com>
2024-02-02 17:18:52 +0100

    Fixed double DB_NAME

--- django-portfolio/portfolio/portfolio/settings.py
+++ django-portfolio/portfolio/portfolio/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_USER'),
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASS'),
         'HOST': config('DB_HOST'),
